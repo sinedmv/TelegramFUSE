0c5b39e659aef0753bfdb1e56554e43070a64a9c
sinedmv <raybeker890@gmail.com>
2024-05-19 11:27:33 +0300

    Finally added tgbot and dependencies

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
-#include <fuse.h>
+#include <stdio.h>
 #include <tgbot/tgbot.h>
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello, world" << std::endl;
+int main() {
+    TgBot::Bot bot("7001811625:AAEg0fFOYm-yJ0ld9CKYEFT-E9xUQyF9lfs");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
 }
